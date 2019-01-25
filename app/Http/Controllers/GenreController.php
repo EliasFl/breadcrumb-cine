67e7d8e5a32e5fc29fb4cdae9c470f2f62f82113
Elias Flores <eliasflores13@outlook.com>
2019-01-25 13:52:46 -0400

    Created the basic methods on the movie and genre controllers

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Genre;
 
 class GenreController extends Controller
 {
-    //
+    public function index()
+    {
+        $genres = Genre::all();
+        
+        return view('genres.index', compact('genres'));
+    }
+
+    public function show(Genre $genre)
+    {
+        return view('genres.show', compact('genre'));
+    }
 }
--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
-    //
+    public function show(Movie $movie)
+    {
+        return view('movies.show', compact('movie'));
+    }
 }
