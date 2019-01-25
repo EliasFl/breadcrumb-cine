72e6aa8619e39ee36d024a8ed84af9a9ba7601d0
Elias Flores <eliasflores13@outlook.com>
2019-01-25 13:34:51 -0400

    Created some fake records to the database for testing purposes

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Movie::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->realText($faker->numberBetween(10, 50)),
+        'image' => $faker->imageUrl(500, 500),
+        'length' => $faker->numberBetween(100, 140),
+        'year' => $faker->numberBetween(1970, 2018)
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Genre;
+use App\Movie;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $genres = factory(Genre::class, 10)->create();
+
+        $genres->each(function ($genre) {
+            factory(Movie::class, 5)->create([
+                'genre_id' => $genre->id
+            ]);
+        });
     }
 }
